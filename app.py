822fa117c213e074d50eedeb5d363c4c7f5a07c9
OlamideShogbamu <olamideshogbamu@gmail.com>
2024-04-17 12:06:29 +0100

    add cmap

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def show_map(hospital):
 def show_c_map(hospital):
     hospital = hospital.capitalize()
     map_url = data_csv[data_csv['Health Facility'].str.capitalize() == hospital]['Map url'].iloc[0]
-    map_url.append(' Go back')
+    map_url = [map_url, 'Go back']
     return map_url
 
 if __name__ == '__main__':
